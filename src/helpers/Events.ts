82499bf78edeb389ff21349c149f5b3daf017fda
Douglas Pacor <douglas@epicquestti.com.br>
2025-01-23 23:26:30 -0300

    doing

--- src/helpers/Events.ts
+++ src/helpers/Events.ts
@@ -1,9 +1,13 @@
 class Events {
-  callbacks = [];
-  nextId = 0;
+  private callbacks: {
+    id: number;
+    eventName: string;
+    caller: any;
+    callback: (data: any) => void;
+  }[] = [];
+  private nextId = 0;
 
-  // emit event
-  emit(eventName, value) {
+  emit(eventName: string, value: any) {
     this.callbacks.forEach((stored) => {
       if (stored.eventName === eventName) {
         stored.callback(value);
@@ -11,8 +15,7 @@ class Events {
     });
   }
 
-  // subscribe to something happening
-  on(eventName, caller, callback) {
+  on(eventName: string, caller: any, callback: (data: any) => void): number {
     this.nextId += 1;
     this.callbacks.push({
       id: this.nextId,
@@ -23,12 +26,11 @@ class Events {
     return this.nextId;
   }
 
-  // remove the subscription
-  off(id) {
+  off(id: number) {
     this.callbacks = this.callbacks.filter((stored) => stored.id !== id);
   }
 
-  unsubscribe(caller) {
+  unsubscribe(caller: any) {
     this.callbacks = this.callbacks.filter(
       (stored) => stored.caller !== caller
     );
