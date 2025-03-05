459af14d33c049c698c63e9d8308419480b60919
Douglas Pacor <douglas@epicquestti.com.br>
2025-03-05 20:01:07 -0300

    doing

--- src/GUI/Inventory/index.ts
+++ src/GUI/Inventory/index.ts
@@ -30,3 +30,24 @@ export class Inventory extends GUI {
     this.money.update(delta);
   }
 }
+
+// render() {
+//   const inventoryItems = gameState.config.inventory || [];
+
+//   inventoryItems.forEach((itemId: string, index: number) => {
+//     const item = gameState.config.items?.find((i: any) => i.id === itemId);
+
+//     if (item && this.slots[index] && !this.slots[index].item) {
+//       this.slots[index].item = item;
+//       this.slots[
+//         index
+//       ].getElement().innerHTML = `<div draggable="true" class="item" data-id="${item.id}">${item.name}</div>`;
+//       this.slots[index]
+//         .getElement()
+//         .querySelector(".item")
+//         ?.addEventListener("dragstart", (e) =>
+//           this.slots[index].handleDragStart(e)
+//         );
+//     }
+//   });
+// }
