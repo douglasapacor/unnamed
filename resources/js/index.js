830f8f5ea72571ec86dba49e8f10259042a15a81
Douglas Pacor <douglas@epicquestti.com.br>
2025-03-06 10:10:37 -0300

    doing

--- resources/js/index.js
+++ resources/js/index.js
@@ -29695,8 +29695,23 @@ void main() {
 
   // src/GUI/Inventory/Character.ts
   var Character = class extends UIComponent {
+    equipament;
+    stats;
     constructor(containerId) {
       super("InventoryCharacter", containerId);
+      this._element.style.display = "grid";
+      this._element.style.gridTemplateColumns = "55% 45%";
+      this._element.style.gridTemplateRows = "100%";
+      this.equipament = document.createElement("div");
+      this.equipament.id = "CharacterEquipament";
+      this.equipament.style.background = "rgb(29, 41, 47)";
+      this.equipament.style.boxShadow = "3px 3px 10px 2px #44403c inset, -3px -3px 10px 2px #44403c inset";
+      this.add(this.equipament);
+      this.stats = document.createElement("div");
+      this.stats.id = "CharacterStats";
+      this.stats.style.background = "rgb(29, 41, 47)";
+      this.stats.style.boxShadow = "3px 3px 10px 2px #44403c inset, -3px -3px 10px 2px #44403c inset";
+      this.add(this.stats);
     }
     update(delta) {
     }
--- src/GUI/Inventory/Character.ts
+++ src/GUI/Inventory/Character.ts
@@ -1,8 +1,28 @@
 import { UIComponent } from "../../lib/UI/UIComponent";
 
 export class Character extends UIComponent {
+  private equipament: HTMLElement;
+  private stats: HTMLElement;
+
   constructor(containerId: string) {
     super("InventoryCharacter", containerId);
+    this._element.style.display = "grid";
+    this._element.style.gridTemplateColumns = "55% 45%";
+    this._element.style.gridTemplateRows = "100%";
+
+    this.equipament = document.createElement("div");
+    this.equipament.id = "CharacterEquipament";
+    this.equipament.style.background = "rgb(29, 41, 47)";
+    this.equipament.style.boxShadow =
+      "3px 3px 10px 2px #44403c inset, -3px -3px 10px 2px #44403c inset";
+    this.add(this.equipament);
+
+    this.stats = document.createElement("div");
+    this.stats.id = "CharacterStats";
+    this.stats.style.background = "rgb(29, 41, 47)";
+    this.stats.style.boxShadow =
+      "3px 3px 10px 2px #44403c inset, -3px -3px 10px 2px #44403c inset";
+    this.add(this.stats);
   }
 
   update(delta: number): void {}
