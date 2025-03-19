a969c2be4177ef0df5aebb075bd78332ebd862b9
Douglas Pacor <douglas@epicquestti.com.br>
2025-03-19 14:50:30 -0300

    doing

--- src/scenes/MainScene.ts
+++ src/scenes/MainScene.ts
@@ -1,7 +1,6 @@
 import { Vec3 } from "cannon-es";
 import { dummy } from "../directories";
 import ActorManager from "../lib/ActorManager";
-import { FireSkill } from "../lib/FIreSkill";
 import GameScene from "../lib/GameScene";
 import { InputController } from "../lib/InputController";
 import Player from "../lib/Player";
@@ -11,7 +10,6 @@ export default class MainScene extends GameScene {
   private terrain!: Terrain;
   private player!: Player;
   private actorManager!: ActorManager;
-  private fire!: FireSkill;
 
   public override preload(): void {
     this.terrain = new Terrain({ scene: this.scene, world: this.world });
@@ -28,13 +26,11 @@ export default class MainScene extends GameScene {
 
   public override create(): void {
     new InputController(this.player);
-    this.fire = new FireSkill({ scene: this.scene });
   }
 
   public override update(delta: number): void {
     this.terrain.update(delta);
     this.player.update(delta);
     this.actorManager.update(this.player, delta);
-    this.fire.update(delta);
   }
 }
